9d64b3ba1c2d5b1df6b1dcba61cecdee20bc68e4
Tiago Ribeiro <tiago.err@outlook.com>
2022-04-29 20:55:04 +0100

    Prepared the TailwindCSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,tsx,ts,js,jsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
